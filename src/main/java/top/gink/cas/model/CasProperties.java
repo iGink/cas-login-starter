55ed6a2a485ead3fde3ace67c84f4bf6612846db
guoxiangkai <guoxiangkai@megvii.com>
2023-10-07 12:48:53 +0800

    添加中文注释

--- src/main/java/top/gink/cas/model/CasProperties.java
+++ src/main/java/top/gink/cas/model/CasProperties.java
@@ -10,15 +10,42 @@ import java.util.List;
 @Getter
 @ConfigurationProperties(prefix = "cas")
 public class CasProperties {
+    /**
+     * 是否启用
+     */
     public boolean enabled;
+    /**
+     * 登陆成功地址
+     */
     public String loginSuccessUrl;
 
+    /**
+     * 基础url
+     */
     public String baseUrl;
+    /**
+     * 服务端url
+     */
     public String serverUrl;
+    /**
+     * 服务端验证url
+     */
     public String serviceValidateUrl;
+    /**
+     * 登录url
+     */
     public String loginUrl;
+    /**
+     * 登出url
+     */
     public String logoutUrl;
+    /**
+     * 回调url
+     */
     public String callbackUrl;
+    /**
+     * 忽略路径
+     */
     public List<String> ignorePath;
 
 }
